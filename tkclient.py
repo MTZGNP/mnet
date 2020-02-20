1f42cd9c10ab5261e7c03e64fbb507ad45ec047e
MTZ.GNP <mutaz.shamaileh2005@gmail.com>
2020-02-20 19:25:29 +0300

    compiled tkclient to exe
    compiled tkclient to exe

--- tkclient.py
+++ tkclient.py
@@ -15,8 +15,12 @@ import requests
 ##                     |___/      |_|
 ##
 ##mutaz.shamaileh2005@gmail.com
-
+def encode(message,pw):
+    return "".join([chr(ord(char)+ord(pw[i%len(pw)])) for i,char in enumerate(message)])
+def decode(message,pw):
+    return "".join([chr(ord(char)-ord(pw[i%len(pw)])) for i,char in enumerate(message)])
 ex=""
+fails=0
 root =tk.Tk()
 name=""
 room="1"
@@ -34,25 +38,34 @@ pwBox.insert(0,pw)
 messageList= tk.Listbox(root)
 messageBox=tk.Entry(root)
 def update_loop():
-	for mess in json.loads(requests.get(url="http://mtzgnp.pythonanywhere.com/get?room=%s" % room).text)[messageList.size():]:
-		messageList.insert(tk.END,"%s : %s" % (mess["name"],mess["message"]))
-	root.after(2000,update_loop)
+    global fails
+    for mess in json.loads(requests.get(url="http://mtzgnp.pythonanywhere.com/get?room=%s" % room).text)[messageList.size()+fails:]:
+        try:
+            messageList.insert(tk.END,"%s : %s" % (mess["name"],decode(mess["message"],pw)))
+        except:
+            fails+=1
+    root.after(2000,update_loop)
 def send():
-	if name =="":
-		messagebox.showerror("error","please enter name")
-		return 1
-	requests.get(url ="http://mtzgnp.pythonanywhere.com/post?room=%s&name=%s&message=%s" %(room,name,messageBox.get()))
+    if name =="":
+        messagebox.showerror("error","please enter name")
+        return 1
+    elif messageBox.get() == "":
+        return 0
+    requests.get(url ="http://mtzgnp.pythonanywhere.com/post?room=%s&name=%s&message=%s" %(room,name,encode(messageBox.get(),pw)))
+    messageBox.delete(0,tk.END)
 def change():
-	global name
-	global room
-	global pw
-	if room !=roomBox.get() and roomBox.get() != "":
-		room=roomBox.get()
-		roomPrev["text"]=room
-	if name != nameBox.get():
-		name=nameBox.get()
-		namePrev["text"]=name
-	pw=pwBox.get()
+    global name
+    global room
+    global pw
+    if room !=roomBox.get() and roomBox.get() != "":
+        room=roomBox.get()
+        roomPrev["text"]=room
+    if name != nameBox.get():
+        name=nameBox.get()
+        namePrev["text"]=name
+    if pw  != pwBox.get():
+            messageList.delete(0,tk.END)
+            pw=pwBox.get()
 changeButton=tk.Button(root,text="change",command=change)
 sendButton=tk.Button(root,text="send",command=send)
 nameLabel.grid(row=0)
@@ -64,8 +77,10 @@ roomPrev.grid(row=1,column=2)
 pwLabel.grid(row=2)
 pwBox.grid(row=2,column=1)
 changeButton.grid(row=2,column=2)
-messageList.grid(row=3,columnspan=3)
-messageBox.grid(row=4,columnspan=3)
-sendButton.grid(row=4,column=2)
+messageList.config(height=40,width=100)
+messageBox.config(width=50)
+messageList.grid(row=3,columnspan=7)
+messageBox.grid(row=4,columnspan=7,rowspan=7)
+sendButton.grid(row=4,column=5)
 update_loop()
 root.mainloop()
